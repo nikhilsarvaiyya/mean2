89413a40449302ce61e0ccdf8021a5a304ac8198
Nikhil Sarvaiyya <nikhils@valueaddsofttech.com>
2017-09-18 16:44:40 +0530

    angular node start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+
+import {MaterialModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+// Imports commented out for brevity
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+// Imports Post Services
+import { PostsService } from './posts.service';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MdButtonModule, 
+    MdCheckboxModule,
+
+    RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
-  providers: [],
+  exports: [
+  	MdButtonModule, 
+  	MdCheckboxModule
+  ],
+  providers: [PostsService], // Add the posts service
   bootstrap: [AppComponent]
 })
 export class AppModule { }
